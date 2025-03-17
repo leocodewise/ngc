d71f9162721cc74f31c117500624423d0ab5c8d9
leocodewise <99092507+leocodewise@users.noreply.github.com>
2025-03-17 11:07:04 -0300

    Finally Something
    hehehe

---
+++ compiler.bat
@@ -0,0 +1,1 @@
+g++ -Wall -O2 -std=c++23 src/main.cpp src/core/utils/FileReader.cpp -o ngc
---
+++ src/core/utils/FileReader.cpp
@@ -0,0 +1,42 @@
+#include "FileReader.h"
+#include <fstream>
+#include <sstream>
+#include <format>
+
+namespace ngc::utils {
+
+std::expected<std::string, std::error_code> FileReader::readFileToString(const std::filesystem::path& filePath) {
+    if (!std::filesystem::exists(filePath)) {
+        return std::unexpected(std::make_error_code(std::errc::no_such_file_or_directory));
+    }
+
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        return std::unexpected(std::make_error_code(std::errc::io_error));
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+std::expected<std::vector<std::string>, std::error_code> FileReader::readFileToLines(const std::filesystem::path& filePath) {
+    if (!std::filesystem::exists(filePath)) {
+        return std::unexpected(std::make_error_code(std::errc::no_such_file_or_directory));
+    }
+
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        return std::unexpected(std::make_error_code(std::errc::io_error));
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(std::move(line));
+    }
+
+    return lines;
+}
+
+} // namespace ngc::utils
